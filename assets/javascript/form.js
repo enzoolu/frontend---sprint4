e6591570fcc58164ef0b02ee6359748f1b92682f
enzo goulart <enzoo.lu@gmail.com>
2023-11-09 15:40:07 -0300

    first commit

---
+++ assets/javascript/form.js
@@ -0,0 +1,32 @@
+document.getElementById('confirmar').addEventListener('click', e => {
+    e.preventDefault();
+
+    dados();
+});
+
+function dados() {
+    let nome = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+
+    if (nome.length < 3 || nome == '') {
+        Swal.fire(
+            {
+                title: 'Erro:',
+                text: 'Preencha o nome corretamente',
+                icon: 'error'
+            });
+    } else if (email == '' || email.indexOf('@') == -1 || email.indexOf('.') == -1 ) {
+        Swal.fire(
+            {
+                title: 'Erro:',
+                text: 'Preencha o email corretamente',
+                icon: 'error'
+            });
+    } else {
+        Swal.fire(
+            {
+                title: 'Dados Salvos',
+                icon: 'success'
+            });
+    };
+};
